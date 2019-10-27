dcee521d0ff8744e88a3c84acce155fde5b1ca79
Bennett-Wendorf <bennettwendorf@yahoo.com>
2019-10-27 12:02:21 -0500

    Began work on user account verification.

---
+++ Windows/CheckAccounts.ps1
@@ -0,0 +1,32 @@
+﻿param(
+    [string]$usersFile
+)
+
+$file = Import-Csv -Path $usersFile
+$localUsers = Get-localUser
+
+Function CheckLocalUsersVsCsv{
+    ForEach($currentLocalUser in $localUsers) {
+        :ToNextLocalUser_LABEL
+        ForEach($user in $file.Users){
+            If($currentLocalUser.Name -eq $user){
+                $localUserMatch = 1
+                break :ToNextLocalUser_LABEL
+            }
+            Else{
+                $localUserMatch = 0
+            }
+        }
+
+        
+        If($localUserMatch){
+            Write-Host "$($currentLocalUser) was found in the csv." 
+        }
+        Else{
+            Write-Host "$($currentLocalUser) was NOT found in the csv."
+        }
+    }
+
+}
+
+CheckLocalUsersVsCsv
