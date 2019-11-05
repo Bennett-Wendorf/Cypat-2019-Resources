ecc80b66ebcf34b6706298f3bf2fd12fccbe60e4
Bennett-Wendorf <bennettwendorf@yahoo.com>
2019-11-04 19:36:30 -0600

    Added check for blank admin name.

--- Windows/AllWindowsBuilds/CheckPassword.ps1
+++ Windows/AllWindowsBuilds/CheckPassword.ps1
@@ -22,52 +22,55 @@ $file = Import-Csv -Path $pathToUsersFile
 
 #Searches through csv and looks for bad passwords.
 ForEach($row in $file){
-    $currentPassword = $row.AdminPasswords
-    if($currentPassword.Length -lt 8){
-        if($enableAdvancedDebugMode){
-            "$($row.Admins)'s password is less than 8 characters long."
-        }
-        $badPass = $true
-    }
-    if($currentPassword -notmatch '!|@|#|%|^|&|$'){
-        if($enableAdvancedDebugMode){
-            "$($row.Admins)'s password does not contain at least one special character."
-        }
-        $badPass = $true
-    }
-    if($currentPassword -notmatch '\d'){
-        if($enableAdvancedDebugMode){
-            "$($row.Admins)'s password does not contain at least one digit [0-9]."
-        }
-        $badPass = $true
-    }
-    if(-not ($currentPassword -cmatch '[A-Z]')){
-        if($enableAdvancedDebugMode){
-            "$($row.Admins)'s password does not contain at least one upper case letter [A-Z]."
-        }
-        $badPass = $true
-    }
-    if(-not ($currentPassword -cmatch '[a-z]')){
-        if($enableAdvancedDebugMode){
-            "$($row.Admins)'s password does not contain at least one lower case letter [a-z]."
-        }
-        $badPass = $true
-    }
-
-    #User is alerted and asked if they want to change a bad password.
-    if($badPass){
-        "Would you like to change $($row.Admins)'s bad password? (y/n)"
-        While($true) {
-            $answer = Read-Host -Prompt 'Please input y or n'
-            If($answer -eq "y"){
-                $localUser = Get-LocalUser -Name $row.Admins
-                Set-LocalUser $localUser -Password $defaultPassword
-                break
+    if($row.Admins.Length -gt 0)
+    {
+        $currentPassword = $row.AdminPasswords
+        if($currentPassword.Length -lt 8){
+            if($enableAdvancedDebugMode){
+                "$($row.Admins)'s password is less than 8 characters long."
             }
-            ElseIf($answer -eq "n") {
-                "Ok"
-                ""
-                break
+            $badPass = $true
+        }
+        if($currentPassword -notmatch '!|@|#|%|^|&|$'){
+            if($enableAdvancedDebugMode){
+                "$($row.Admins)'s password does not contain at least one special character."
+            }
+            $badPass = $true
+        }
+        if($currentPassword -notmatch '\d'){
+            if($enableAdvancedDebugMode){
+                "$($row.Admins)'s password does not contain at least one digit [0-9]."
+            }
+            $badPass = $true
+        }
+        if(-not ($currentPassword -cmatch '[A-Z]')){
+            if($enableAdvancedDebugMode){
+                "$($row.Admins)'s password does not contain at least one upper case letter [A-Z]."
+            }
+            $badPass = $true
+        }
+        if(-not ($currentPassword -cmatch '[a-z]')){
+            if($enableAdvancedDebugMode){
+                "$($row.Admins)'s password does not contain at least one lower case letter [a-z]."
+            }
+            $badPass = $true
+        }
+
+        #User is alerted and asked if they want to change a bad password.
+        if($badPass){
+            "Would you like to change $($row.Admins)'s bad password? (y/n)"
+            While($true) {
+                $answer = Read-Host -Prompt 'Please input y or n'
+                If($answer -eq "y"){
+                    $localUser = Get-LocalUser -Name $row.Admins
+                    Set-LocalUser $localUser -Password $defaultPassword
+                    break
+                }
+                ElseIf($answer -eq "n") {
+                    "Ok"
+                    ""
+                    break
+                }
             }
         }
     }
